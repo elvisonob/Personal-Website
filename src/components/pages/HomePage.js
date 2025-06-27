0fc831369fbe68c59b48740e77a14ec7d364d129
elvisonob <elvisonob@yahoo.com>
2025-06-27 09:38:28 -0700

    Redesigning

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,16 +4,13 @@ import classes from './HomePage.module.css';
 const HomePage = () => {
   return (
     <div className={classes.container}>
-      <div className={classes.text}>
-        <h1 className={classes['name-title']}>Elvis</h1>
-        <h2 className={classes['role-title']}>Software Developer</h2>
-        <h3 className={classes['location-name']}>based in United Kingdom</h3>
-        <p className={classes['current-skills']}>
-          HTML/CSS - JavaScript - TypeScript - React - NextJS - NodeJS -
-          ExpressJS - API creation(REST & GraphQL) - OOP - Git - GitHub - SQL -
-          NoSQL
-        </p>
-      </div>
+      <h1 className={classes['name-title']}>Elvis</h1>
+      <h2 className={classes['role-title']}>Software Developer</h2>
+      <h3 className={classes['location-name']}>United Kingdom</h3>
+      <p className={classes['current-skills']}>
+        HTML/CSS - JavaScript - TypeScript - React - NextJS - NodeJS - ExpressJS
+        - API creation(REST & GraphQL) - OOP - Git - GitHub - SQL - NoSQL
+      </p>
     </div>
   );
 };
