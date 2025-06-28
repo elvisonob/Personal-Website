06aab37f1596e8fddac6cb181d742bb97c4cf978
elvisonob <elvisonob@yahoo.com>
2025-06-28 10:21:42 -0700

    redesigning portfolio css

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './About.module.css';
-import image from '../../images/coding-pic-edit.jpg';
+import image from '../../images/portfolio-pic.jpg';
 import github from '../../images/github.jpg';
 import linkedin from '../../images/linkedin.png';
 import ElvisCV from '../../images/Elvis IT Resume.pdf';
@@ -8,74 +8,44 @@ import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
-    <div className={classes.AboutMeContainer}>
-      <div className={classes.leftHandSide}>
-        <section className={classes.content}>
-          <div className={classes.aboutMeTop}>About Me</div>
-          <p className={classes.aboutMe}>
-            Hello dear, I am Elvis, an enthusiastic Software Developer. I made a
-            deliberate decision to specialize in Software Development from an
-            Electrical and Electronics Engineering background. This choice was
-            driven by the thrill I feel when writing a simple line of code that
-            transforms into an immediately usable art; and recognizing that
-            technology serves as a linchpin in our modern society, I feel more
-            fulfilled contributing positively to the advancement of our world
-            through information technology. Furthermore, I love challenging and
-            problem-solving tasks. I enjoy creating things from scratch and
-            building on preexisting things to make them more refined. Software
-            development has provided the avenue for me to explore such ideas,
-            and above all, I like learning new things and applying the knowledge
-            to everyday scenarios.
-          </p>
-        </section>
+    <div className={classes.container}>
+      <div className={classes.information}>
+        <h2>About Me</h2>
+        <p>
+          Hi, I am Elvis — a passionate Software Developer with a background in
+          Electrical and Electronics Engineering. I transitioned into software
+          development driven by the excitement of turning ideas into functional
+          solutions through code. I thrive on solving problems, building
+          products (more information on my portfolio) from the ground up, and
+          refining existing systems. For me, software is a powerful way to
+          contribute to the world, and I am constantly learning to stay ahead in
+          this ever-evolving field.
+        </p>
       </div>
-      <div className={classes.rightHandSide}>
-        <div className={classes.picture}>
-          <div></div>
-          <img
-            className={classes.resize}
-            src={image}
-            width="100%"
-            height="80%"
-            alt=""
-          />
-          <div></div>
-        </div>
-        <div className={classes.socialMediaLink}>
-          <div className={classes.linkedin}>
+      <div className={classes.leftMenu}>
+        <div className={classes.leftMenuOptions}>
+          <img className={classes.myimage} src={image} alt="" />
+
+          <div className={classes.socialMedia}>
+            <Link
+              to={{ pathname: 'https://github.com/elvisonob' }}
+              target="_blank"
+            >
+              <img src={github} height="100%" width="100%" alt="" />
+            </Link>
             <Link
               to={{
                 pathname: 'https://www.linkedin.com/in/elvis-ono-5bb4081b6/',
               }}
               target="_blank"
             >
-              <img
-                className={classes.linkedinImage}
-                src={linkedin}
-                width="100%"
-                height="100%"
-                alt=""
-              />
+              <img src={linkedin} height="100%" width="100%" alt="" />
             </Link>
           </div>
-          <div className={classes.github}>
-            <Link
-              to={{ pathname: 'https://github.com/elvisonob' }}
-              target="_blank"
-            >
-              <img
-                className={classes.gitHubImage}
-                src={github}
-                width="100%"
-                height="100%"
-                alt=""
-              />
-            </Link>
-          </div>
+          <a className={classes.CV} href={ElvisCV} download="Resume">
+            Download CV
+          </a>
         </div>
-        <a className={classes.cv} href={ElvisCV} download="Resume">
-          Download CV
-        </a>
       </div>
     </div>
   );
