074f3b7f0a2867a0d47a453af1810d73f4b92d0e
elvisonob <elvisonob@yahoo.com>
2025-08-07 10:24:07 -0700

    Update

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     <div className={classes.container}>
       <h1 className={classes['name-title']}>Elvis</h1>
       <h2 className={classes['role-title']}>Software Developer</h2>
-      <h3 className={classes['location-name']}>United States</h3>
+      <h3 className={classes['location-name']}>United Kingdom</h3>
       <p className={classes['current-skills']}>
         HTML/CSS - JavaScript - TypeScript - React - NextJS - NodeJS - ExpressJS
         - API creation(REST & GraphQL) - OOP - Git - GitHub - SQL - NoSQL
