ef7f81f36d6eb36e80974fd7829d4e56d3cec9aa
elvisonob <elvisonob@yahoo.com>
2026-09-02 07:09:52 +0100

    Updated About Me section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,15 +12,13 @@ const About = () => {
       <div className={classes.information}>
         <h2>About Me</h2>
         <p>
-          Hi, I'm Elvis — a Software Developer with a foundation in Electrical
-          and Electronics Engineering. I transitioned into software development
-          driven by the excitement of turning ideas into functional solutions
-          that improves organisations and individuals. I thrive on solving
-          problems, building products (more information on my portfolio page)
-          from the ground up, and refining existing systems. For me, coding is
-          not just a craft but a powerful way to make a positive impact. I'm
-          constantly learning and evolving to stay ahead in this fast-moving
-          field.
+          Hi, I'm Elvis — a Software Developer who is driven by the excitement
+          of turning ideas into functional solutions that empowers organisations
+          and individuals. I thrive on solving problems, building products (more
+          information on my portfolio page) from the ground up, and refining
+          existing systems. For me, coding is not just a craft but a powerful
+          way to make a good impact. I'm constantly learning and evolving to
+          stay ahead in this fast-moving field.
         </p>
       </div>
       <div className={classes.leftMenu}>
